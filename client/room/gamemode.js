0ec3bd6ca2293629128e31c154339a5db39e9846
BTZ-OFFICIAL <baktybektagaev50@gmail.com>
2024-11-10 22:30:19 +0300

    Update gamemode.js

--- client/room/gamemode.js
+++ client/room/gamemode.js
@@ -35,13 +35,15 @@ room.Damage.FriendlyFire = false;
 //room.Map.Rotation = MAP_ROTATION;
 room.BreackGraph.OnlyPlayerBlocksDmg = room.GameMode.Parameters.GetBool("PartialDesruction");
 room.BreackGraph.WeakBlocks = room.GameMode.Parameters.GetBool("LoosenBlocks");
-// set blue var inventory = Inventory.GetContext(Blue);
+// set blue 
+inventory = Inventory.GetContext(Blue);
 inventory.Main.Value = false;
 inventory.Secondary.Value = false;
 inventory.Melee.Value = false;
 inventory.Explosive.Value = false;
 inventory.Build.Value = false;
-// set red var inventory = Inventory.GetContext(Red);
+// set red 
+inventory = Inventory.GetContext(Red);
 inventory.MainInfinity.Value = true;
 inventory.SecondaryInfinity.Value = true;
 inventory.ExplosiveInfinity.Value = true;
